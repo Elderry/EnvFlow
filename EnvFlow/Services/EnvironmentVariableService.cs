dab7ed6fe8813f0dd80a594341db5ac345270a0e
Ruiyang Li <Elderry@outlook.com>
2025-12-30 18:13:07 +0800

    Use Windows Registry for environment variable management

--- EnvFlow/Services/EnvironmentVariableService.cs
+++ EnvFlow/Services/EnvironmentVariableService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.Win32;
 
 namespace EnvFlow.Services;
 
@@ -11,12 +12,31 @@ public class EnvironmentVariableService
     {
         var variables = new Dictionary<string, string>();
         
-        foreach (var key in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User).Keys)
+        try
         {
-            var value = Environment.GetEnvironmentVariable(key.ToString()!, EnvironmentVariableTarget.User);
-            if (value != null)
+            using var key = Registry.CurrentUser.OpenSubKey(@"Environment");
+            if (key != null)
             {
-                variables[key.ToString()!] = value;
+                foreach (var valueName in key.GetValueNames())
+                {
+                    var value = key.GetValue(valueName, "", RegistryValueOptions.DoNotExpandEnvironmentNames);
+                    if (value != null)
+                    {
+                        variables[valueName] = value.ToString()!;
+                    }
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // Fallback to Environment API if registry access fails
+            foreach (var k in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User).Keys)
+            {
+                var value = Environment.GetEnvironmentVariable(k.ToString()!, EnvironmentVariableTarget.User);
+                if (value != null)
+                {
+                    variables[k.ToString()!] = value;
+                }
             }
         }
         
@@ -29,12 +49,16 @@ public class EnvironmentVariableService
         
         try
         {
-            foreach (var key in Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine).Keys)
+            using var key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment");
+            if (key != null)
             {
-                var value = Environment.GetEnvironmentVariable(key.ToString()!, EnvironmentVariableTarget.Machine);
-                if (value != null)
+                foreach (var valueName in key.GetValueNames())
                 {
-                    variables[key.ToString()!] = value;
+                    var value = key.GetValue(valueName, "", RegistryValueOptions.DoNotExpandEnvironmentNames);
+                    if (value != null)
+                    {
+                        variables[valueName] = value.ToString()!;
+                    }
                 }
             }
         }
@@ -78,21 +102,101 @@ public class EnvironmentVariableService
 
     public void SetUserVariable(string name, string value)
     {
-        Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.User);
+        using var key = Registry.CurrentUser.OpenSubKey(@"Environment", true);
+        if (key != null)
+        {
+            var valueKind = IsExpandableString(value) ? RegistryValueKind.ExpandString : RegistryValueKind.String;
+            key.SetValue(name, value, valueKind);
+        }
+        
+        // Notify system of environment change
+        NotifyEnvironmentChange();
     }
 
     public void SetSystemVariable(string name, string value)
     {
-        Environment.SetEnvironmentVariable(name, value, EnvironmentVariableTarget.Machine);
+        using var key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment", true);
+        if (key != null)
+        {
+            var valueKind = IsExpandableString(value) ? RegistryValueKind.ExpandString : RegistryValueKind.String;
+            key.SetValue(name, value, valueKind);
+        }
+        
+        // Notify system of environment change
+        NotifyEnvironmentChange();
     }
 
     public void DeleteUserVariable(string name)
     {
-        Environment.SetEnvironmentVariable(name, null, EnvironmentVariableTarget.User);
+        using var key = Registry.CurrentUser.OpenSubKey(@"Environment", true);
+        if (key != null)
+        {
+            key.DeleteValue(name, false);
+        }
+        
+        // Notify system of environment change
+        NotifyEnvironmentChange();
     }
 
     public void DeleteSystemVariable(string name)
     {
-        Environment.SetEnvironmentVariable(name, null, EnvironmentVariableTarget.Machine);
+        using var key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment", true);
+        if (key != null)
+        {
+            key.DeleteValue(name, false);
+        }
+        
+        // Notify system of environment change
+        NotifyEnvironmentChange();
+    }
+
+    private bool IsExpandableString(string value)
+    {
+        // Check if the value contains environment variable references like %USERPROFILE%, %SystemRoot%, etc.
+        return value.Contains('%');
+    }
+
+    private void NotifyEnvironmentChange()
+    {
+        // This notifies Windows that environment variables have changed
+        // so other processes can pick up the changes
+        try
+        {
+            const int HWND_BROADCAST = 0xffff;
+            const int WM_SETTINGCHANGE = 0x001A;
+            
+            // Using P/Invoke to notify system
+            SendMessageTimeout(
+                (IntPtr)HWND_BROADCAST,
+                WM_SETTINGCHANGE,
+                IntPtr.Zero,
+                "Environment",
+                SendMessageTimeoutFlags.SMTO_ABORTIFHUNG,
+                5000,
+                out _);
+        }
+        catch
+        {
+            // Ignore errors in notification
+        }
+    }
+
+    [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SendMessageTimeout(
+        IntPtr hWnd,
+        int Msg,
+        IntPtr wParam,
+        string lParam,
+        SendMessageTimeoutFlags fuFlags,
+        uint uTimeout,
+        out IntPtr lpdwResult);
+
+    [Flags]
+    private enum SendMessageTimeoutFlags : uint
+    {
+        SMTO_NORMAL = 0x0,
+        SMTO_BLOCK = 0x1,
+        SMTO_ABORTIFHUNG = 0x2,
+        SMTO_NOTIMEOUTIFNOTHUNG = 0x8
     }
 }
